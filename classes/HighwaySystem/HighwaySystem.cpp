418fc0a7d7055c82d6e49aedd3168a76bf296b1e
eric bryant <ericdbryant@yahoo.com>
2021-07-03 13:37:05 -0400

    HighwaySystem slimdown 2
    class

--- classes/HighwaySystem/HighwaySystem.cpp
+++ classes/HighwaySystem/HighwaySystem.cpp
@@ -13,37 +13,21 @@ HighwaySystem::HighwaySystem(std::string &line)
 {	std::ifstream file;
 	// parse systems.csv line
 	size_t NumFields = 6;
-	std::string country_str, tier_str, level_str;
-	std::string* fields[6] = {&systemname, &country_str, &fullname, &color, &tier_str, &level_str};
+	std::string dummy;
+	std::string* fields[6] = {&systemname, &dummy, &dummy, &dummy, &dummy, &dummy};
 	split(line, fields, NumFields, ';');
-	is_valid = 1;
-
-	// Removing sanity checks, as TMMinimal is to be tested
-	// with a HighwayData commit known to have valid data
-
-	// Tier
-	char *endptr;
-	tier = strtol(tier_str.data(), &endptr, 10);
-	// Level
-	level = level_str[0];
 
 	std::cout << systemname << '.' << std::flush;
 
 	// read chopped routes CSV
 	file.open(Args::highwaydatapath+"/hwy_data/_systems"+"/"+systemname+".csv");
-	if (!file) std::cout << "ERROR: Could not open "+Args::highwaydatapath+"/hwy_data/_systems"+"/"+systemname+".csv" << std::endl;
-	else {	getline(file, line); // ignore header line
+		getline(file, line); // ignore header line
 		while(getline(file, line))
 		{	if (line.empty()) continue;
 			// trim DOS newlines & trailing whitespace
 			while ( strchr("\r\t ", line.back()) ) line.pop_back();
-			Route* r = new Route(line, this);
-				   // deleted on termination of program
-			if (r->root.size()) route_list.push_back(r);
-			else {	std::cout << "ERROR: Unable to find root in " + systemname +".csv line: ["+line+"]" << std::endl;
-				delete r;
-			     }
+			route_list.push_back(new Route(line, this));
+					     // deleted on termination of program
 		}
-	     }
 	file.close();
 }
--- classes/HighwaySystem/HighwaySystem.h
+++ classes/HighwaySystem/HighwaySystem.h
@@ -10,23 +10,11 @@ class HighwaySystem
 	that represents the collection of highways within a system.
 
 	See Route for information about the fields of a .csv file
-
-	With the implementation of three levels of systems (active,
-	preview, devel), added parameter and field here, to be stored in
-	DB
 	*/
 
 	public:
 	std::string systemname;
-	std::string fullname;
-	std::string color;
-	short tier;
-	char level; // 'a' for active, 'p' for preview, 'd' for devel
-
 	std::vector<Route*> route_list;
-	std::unordered_set<std::string>listnamesinuse, unusedaltroutenames;
-	std::mutex lniu_mtx, uarn_mtx;
-	bool is_valid;
 
 	static std::list<HighwaySystem*> syslist;
 	static std::list<HighwaySystem*>::iterator it;
